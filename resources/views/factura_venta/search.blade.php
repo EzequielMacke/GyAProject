b36d68784aca0b54d93cd08fb5f6cb1001140069
EzequielMacke <ezequiel.macke@gmail.com>
2026-03-23 14:51:21 -0300

    Reestructuracion gya

--- resources/views/factura_venta/search.blade.php
+++ resources/views/factura_venta/search.blade.php
@@ -232,7 +232,7 @@
                 </div>
                 @else
                 <div class="cards-grid" id="cardsGrid">
-                    @foreach($presupuestos->reverse() as $i => $pres)
+                    @foreach($presupuestos->reverse()->values() as $i => $pres)
                     @php
                         $badgeClass = match($estados_btn[$pres->estado] ?? '') {
                             'danger'  => 'badge-danger',
