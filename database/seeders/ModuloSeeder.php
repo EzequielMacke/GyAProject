8438fa4dc1ae4f981d28ece5f7cd4b1cd094dd85
EzequielMacke <ezequiel.macke@gmail.com>
2026-07-24 17:32:40 -0300

    GyAProject

--- database/seeders/ModuloSeeder.php
+++ database/seeders/ModuloSeeder.php
@@ -12,25 +12,39 @@ class ModuloSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('modulos')->insert([
-            ['id' => 1,  'nombre' => 'are_ing',      'descripcion' => 'Area de Ingeniería',                          'estado' => 1],
-            ['id' => 2,  'nombre' => 'are_dep',      'descripcion' => 'Area de Deposito',                            'estado' => 1],
-            ['id' => 3,  'nombre' => 'ped_obr_ing',  'descripcion' => 'Pedido para obra -Ingenieria',                'estado' => 1],
-            ['id' => 4,  'nombre' => 'ped_obr_dep',  'descripcion' => 'Pedido para obra -Deposito',                  'estado' => 1],
-            ['id' => 5,  'nombre' => 'man',          'descripcion' => 'Mantenimiento',                               'estado' => 1],
-            ['id' => 6,  'nombre' => 'ins',          'descripcion' => 'Insumos',                                     'estado' => 1],
-            ['id' => 7,  'nombre' => 'obr',          'descripcion' => 'Obras',                                       'estado' => 1],
-            ['id' => 8,  'nombre' => 'usu',          'descripcion' => 'Usuarios',                                    'estado' => 1],
-            ['id' => 9,  'nombre' => 'per',          'descripcion' => 'Permisos',                                    'estado' => 1],
-            ['id' => 10, 'nombre' => 'pre_apr_ing',  'descripcion' => 'Presupuestos Aprobados - Ingenieria',         'estado' => 1],
-            ['id' => 11, 'nombre' => 'are_adm',      'descripcion' => 'Area de Adminitracion',                       'estado' => 1],
-            ['id' => 12, 'nombre' => 'pre_apr_adm',  'descripcion' => 'Presupuestos Aprobados - Administracion',     'estado' => 1],
-            ['id' => 13, 'nombre' => 'val_pre_apr',  'descripcion' => 'Validacion de Presupuestos Aprobados',        'estado' => 1],
-            ['id' => 14, 'nombre' => 'age_tra',      'descripcion' => 'Agendamiento de trabajos',                    'estado' => 1],
-            ['id' => 15, 'nombre' => 'ges_tra',      'descripcion' => 'Gestion de Trabajos',                         'estado' => 1],
-            ['id' => 16, 'nombre' => 'ges_tra_asig', 'descripcion' => 'Asignaciones - Gestion de Trabajos',          'estado' => 1],
-            ['id' => 17, 'nombre' => 'her',          'descripcion' => 'Herramientas',                                'estado' => 1],
-            ['id' => 18, 'nombre' => 'gen_doc',      'descripcion' => 'Generador de Documentos',                     'estado' => 1],
-        ]);
+        $modulos = [
+            ['nombre' => 'obr',     'descripcion' => 'Obra',                     'estado' => 1],
+            ['nombre' => 'dir',     'descripcion' => 'Directorio',               'estado' => 1],
+            ['nombre' => 'pre_apr', 'descripcion' => 'Presupuesto Aprobado',     'estado' => 1],
+            ['nombre' => 'ped_ins', 'descripcion' => 'Pedidos de Insumos',       'estado' => 1],
+            ['nombre' => 'con',     'descripcion' => 'Contactos',                'estado' => 1],
+            ['nombre' => 'inv',     'descripcion' => 'Inventario',               'estado' => 1],
+            ['nombre' => 'fac',     'descripcion' => 'Facturacion',              'estado' => 1],
+            ['nombre' => 'dat',     'descripcion' => 'Datos de Obra',            'estado' => 1],
+            ['nombre' => 'tab',     'descripcion' => 'Tables',                   'estado' => 1],
+            ['nombre' => 'equ',     'descripcion' => 'Equipos',                  'estado' => 1],
+            ['nombre' => 'veh',     'descripcion' => 'Vehiculos',                'estado' => 1],
+            ['nombre' => 'per',     'descripcion' => 'Permisos',                 'estado' => 1],
+            ['nombre' => 'usu',     'descripcion' => 'Usuarios',                 'estado' => 1],
+            ['nombre' => 'her',     'descripcion' => 'Herramientas',             'estado' => 1],
+            ['nombre' => 'man',     'descripcion' => 'Mantenimiento',            'estado' => 1],
+            ['nombre' => 'rep',     'descripcion' => 'Reportes',                 'estado' => 1],
+            ['nombre' => 'ins',     'descripcion' => 'Insumos',                  'estado' => 1],
+            ['nombre' => 'kit',     'descripcion' => 'Kits',                     'estado' => 1],
+            ['nombre' => 'pre_ped', 'descripcion' => 'Preparar Pedidos',         'estado' => 1],
+            ['nombre' => 'asi_ord', 'descripcion' => 'Asignacion de OT',         'estado' => 1],
+            ['nombre' => 'car_fac', 'descripcion' => 'Facturacion',              'estado' => 1],
+            ['nombre' => 'sit_ava', 'descripcion' => 'Situacion de Avances',     'estado' => 1],
+            ['nombre' => 'pro',     'descripcion' => 'Programa de Mejoramiento', 'estado' => 1],
+            ['nombre' => 'sol',     'descripcion' => 'Soluciones',               'estado' => 1],
+            ['nombre' => 'bib',     'descripcion' => 'Bibliografía',             'estado' => 1],
+            ['nombre' => 'pla',     'descripcion' => 'Plantilla',                'estado' => 1],
+        ];
+
+        foreach ($modulos as $modulo) {
+            if (! DB::table('modulos')->where('nombre', $modulo['nombre'])->exists()) {
+                DB::table('modulos')->insert($modulo);
+            }
+        }
     }
 }
